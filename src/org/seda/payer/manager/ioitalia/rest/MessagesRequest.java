163fc4607d5cc894f815a046857d63ff67be27b3
ggiacani <giorgia.giacani@maggioli.it>
2023-03-10 16:37:05 +0100

    PGNTMGR-1

--- src/org/seda/payer/manager/ioitalia/rest/MessagesRequest.java
+++ src/org/seda/payer/manager/ioitalia/rest/MessagesRequest.java
@@ -4,30 +4,30 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 public class MessagesRequest {
 	
-	protected int time_to_live = 3600;
+//	protected int time_to_live = 3600;
 	protected Content content;
-	@JsonInclude(JsonInclude.Include.NON_NULL)
-	protected DefaultAddresses default_addresses;
+//	@JsonInclude(JsonInclude.Include.NON_NULL)
+//	protected DefaultAddresses default_addresses;
 	protected String fiscal_code;
 	
-	public int getTime_to_live() {
-		return time_to_live;
-	}
-	public void setTime_to_live(int time_to_live) {
-		this.time_to_live = time_to_live;
-	}
+//	public int getTime_to_live() {
+//		return time_to_live;
+//	}
+//	public void setTime_to_live(int time_to_live) {
+//		this.time_to_live = time_to_live;
+//	}
 	public Content getContent() {
 		return content;
 	}
 	public void setContent(Content content) {
 		this.content = content;
 	}
-	public DefaultAddresses getDefault_addresses() {
-		return default_addresses;
-	}
-	public void setDefault_addresses(DefaultAddresses default_addresses) {
-		this.default_addresses = default_addresses;
-	}
+//	public DefaultAddresses getDefault_addresses() {
+//		return default_addresses;
+//	}
+//	public void setDefault_addresses(DefaultAddresses default_addresses) {
+//		this.default_addresses = default_addresses;
+//	}
 	public String getFiscal_code() {
 		return fiscal_code;
 	}
