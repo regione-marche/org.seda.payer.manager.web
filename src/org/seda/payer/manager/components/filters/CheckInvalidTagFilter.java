22575d7b2f7a81bb010b31b3447f32661ab8b388
simone.brunori <simone.brunori@newsoris.pagonet.it>
2023-06-06 17:18:17 +0200

    FIX checkInvalidTagFilter

--- src/org/seda/payer/manager/components/filters/CheckInvalidTagFilter.java
+++ src/org/seda/payer/manager/components/filters/CheckInvalidTagFilter.java
@@ -46,6 +46,7 @@ public class CheckInvalidTagFilter implements Filter {
 		if(allowElementsString.length()>0)
 			allowElements = allowElementsString.split(";");
 		
+		
 		chain.doFilter(new FilteredRequest(request, allowElements), response);
 		return;
 	}
@@ -69,7 +70,16 @@ public class CheckInvalidTagFilter implements Filter {
         public String sanitize(String input) {
             String result = "";
 
-            result = policy.sanitize(input);
+            result = policy.sanitize(input)	            		
+            		.replace("&#64;", "@")
+            		.replace("&#33;", "!")
+            		.replace("&#35;", "#")
+            		.replace("&#36;", "$")
+            		.replace("&#37;", "%")
+            		.replace("&#38;", "&")
+            		.replace("&#42;", "*")
+            		.replace("&#43;", "+");
+            
             
             return result;
         }
