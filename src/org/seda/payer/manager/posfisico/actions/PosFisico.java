c264eccc36c1abbee3896b0240cf1ea4d69c67a0
Dario Licci <dario.licci@gmail.com>
2023-09-29 14:40:04 +0200

    PGNTMGR-18

--- src/org/seda/payer/manager/posfisico/actions/PosFisico.java
+++ src/org/seda/payer/manager/posfisico/actions/PosFisico.java
@@ -41,7 +41,7 @@ public class PosFisico extends BaseManagerAction {
 				
 				PropertiesTree configuration = (PropertiesTree)context.getAttribute(ManagerKeys.CONTEXT_PROPERTIES_TREE);
 				
-				// inizializza le proprietà
+				// inizializza le proprietï¿½
 				UserBean user = (UserBean)session.getAttribute(SignOnKeys.USER_BEAN);				
 				String sNome = user.getNome();
 				String sCognome = user.getCognome();
@@ -49,7 +49,7 @@ public class PosFisico extends BaseManagerAction {
 				
 				//setto il tokencsrf
 				String tokenName = CSRFContext.getInstance().getTokenName();
-				String tokenValue = CSRFUtil.getTokenFromSession(session, CSRFContext.getInstance().getTokenNameSave());
+				String tokenValue = CSRFUtil.getTokenFromSession(session, tokenName);
 				String tokenCsrf = tokenName + "=" + tokenValue;
 				
 				//recupero la url corrente
